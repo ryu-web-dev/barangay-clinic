8e124e28f2e7fa37724c117c914c642ca8e00dae
Ryuzaki <stylishgaming01@gmail.com>
2026-03-03 11:42:23 +0800

    Deployment ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -36,7 +37,7 @@ app.use('/api/treatments', require('./routes/treatments'));
 // We will add the actual routes (Patients, Inventory, etc.) right here later.
 
 // Start the Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
